a0630c12b2fbc5719f945ed030dc55c2046e7552
IulianBotnari <iulianbotnari93@gmail.com>
2024-10-16 15:50:04 +0200

    Add Snack 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,22 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+let studentName = "Marco Lanci"
+
+function recuperaClasse(studName, arr) {
+
+arr.forEach(element => {
+  if (studName === element.name) {
+
+     console.log(element.class)
+
+    
+  }
+  
+});
+  
+}
+
+
+let whichClass = recuperaClasse(studentName, students)
+
