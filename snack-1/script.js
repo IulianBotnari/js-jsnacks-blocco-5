ebcca76cc7863d600584eb3ae4669d6eb7f14242
IulianBotnari <iulianbotnari93@gmail.com>
2024-10-16 15:17:11 +0200

    add snack1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,11 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+
+console.log(names);
+
+names.forEach((element) =>{
+    console.log(element);
+    
+})
